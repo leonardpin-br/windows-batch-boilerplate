17677324b44e53dfd09997df7a21ec1088542b84
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2023-02-21 09:33:14 -0300

    Added useful functions

--- src/Functions.bat
+++ src/Functions.bat
@@ -68,6 +68,101 @@ GOTO :EOF
     GOTO :EOF
 
 
+:create_string
+    @REM Creates a string.
+    @REM
+    @REM %~1: Variable name.
+    @REM %~2: String inside quotes.
+    @REM
+    @REM How to use this function:
+    @REM    CALL :create_string variable_name "Everything INSIDE the quotation marks."
+    @REM    ECHO !variable_name!
+    @REM    ECHO !variable_name.length!
+
+    SETLOCAL
+
+        @REM Local variable to hold the string length.
+        SET string.length=0
+
+        @REM Removes from the count the extra bytes.
+        SET /A takeaway=2
+
+        @REM The string inside quotation marks.
+        SET string=%~2
+
+        @REM useback will remove the outside quotation marks, leaving any internal ones.
+        FOR /F "useback tokens=*" %%i IN ('!string!') DO (
+
+            @REM The variable is set with the string without quotes.
+            SET string=%%~i
+
+        )
+
+        @REM Creates a temporary file and writes the string without quotations inside it.
+        ECHO %~2> %TEMP%\tempfile.txt
+
+        @REM %%j will be C:\Users\leonardo\AppData\Local\Temp\tempfile.txt.
+        FOR %%j IN ( %TEMP%\tempfile.txt ) DO (
+
+            IF !string! EQU %%j (
+                SET /A takeaway=0
+            )
+
+            @REM %%~z means the file size (like 41 bytes).
+            @REM %%~zj means the file size of C:\Users\leonardo\AppData\Local\Temp\tempfile.txt.
+            SET /A string.length=%%~zj - !takeaway!
+
+        )
+
+        @REM Deletes the temporary file.
+        DEL %TEMP%\tempfile.txt
+
+    ( ENDLOCAL & REM
+        IF "%~1" NEQ "" SET %~1=%string%
+        IF "%~1.length" NEQ "" SET %~1.length=%string.length%
+    )
+    GOTO :EOF
+
+
+:header
+    @REM Prints a message that is easy to read.
+
+    SETLOCAL
+
+        ECHO.
+        ECHO %~1
+        ECHO ===================================================================
+
+    ENDLOCAL
+    GOTO :EOF
+
+
+:multiline_string_into_variable
+    @REM Inserts a multiline string into a variable
+
+    SETLOCAL
+
+        CALL :header "Inserts a multiline string into a variable"
+        @REM Two empty lines are required below this line.
+        SET LF=^
+
+
+        @REM Two empty lines are required above this line.
+        SET multiLine=This is a;!LF!
+        SET multiLine=!multiLine!text with 2 lines%%!LF!
+        SET multiLine=!multiLine!!LF!
+        SET multiLine=!multiLine!1 empty line and a 4 line^^!
+
+        @REM Prints the line on the screen.
+        ECHO !multiLine!
+
+        @REM Sends the multiline to a file.
+        ECHO !multiLine!> multiline_variable_into_file.txt
+
+    ENDLOCAL
+    GOTO :EOF
+
+
 :print_message
     @REM Prints a message that is easy to read.
 
@@ -79,6 +174,41 @@ GOTO :EOF
         ECHO %~1
         ECHO.
         ECHO ===================================================================
+
+    ENDLOCAL
+    GOTO :EOF
+
+
+:read_file_content_into_variable
+    @REM Reads the file content into a variable.
+
+    SETLOCAL
+
+        CALL :header "Reads the file's content into a variable"
+
+        @REM OPTION 1 (only one that really works!):
+        @REM -------------------------------------------------------------------
+        @REM Avoids problems with special characters like ; % !
+        SET /P file_content=<multiline_variable_into_file.txt
+
+
+        @REM @REM OPTION 2 (This is bad and should not be used!)
+        @REM @REM -------------------------------------------------------------------
+        @REM @REM Has problems with special characters like !
+        @REM @REM Does not keep the empty lines.
+
+        @REM @REM Two empty lines are required below this line.
+        @REM SET LF=^
+
+
+        @REM @REM Two empty lines are required above this line.
+        @REM FOR /F "tokens=* delims=" %%i IN (multiline_variable_into_file.txt) DO (
+        @REM     SET "file_content=!file_content!%%i!LF!"
+        @REM )
+
+
+
+        ECHO !file_content!
 
     ENDLOCAL
     GOTO :EOF
